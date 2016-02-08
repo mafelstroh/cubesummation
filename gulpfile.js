33f88ebd3926bf43e55d791f65b5117ae9da01ba
Manuel Felipe Stroh <donmafelstroh@gmail.com>
2016-02-08 17:15:01 -0500

    004 - Fixing default style and changes to the front end

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,7 @@ elixir(function(mix) {
 elixir(function(mix) { 
     var bootstrapPath = 'node_modules/bootstrap-sass/assets'; 
 
-    mix.sass('app.scss') 
+    mix.sass(['app.scss', 'layout.scss']) 
         .copy(bootstrapPath + '/fonts', 'public/fonts') 
         .copy(bootstrapPath + '/javascripts/bootstrap.min.js', 'public/js'); 
 });
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,34 +7,36 @@
 </head>
 <body>
 
-<nav class="navbar navbar-default">
-    <div class="container-fluid">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">Cube summation</a>
-        </div>
+   <div class="container">
+      <div class="header clearfix">
+        <nav>
+          <ul class="nav nav-pills pull-right">
+            <li role="presentation"><a href="#">About</a></li>
+            <li role="presentation"><a href="#">Contact</a></li>
+          </ul>
+        </nav>
+        <h1 class="text-muted">Cube Summation</h1>
+      </div>
 
+      <div class="jumbotron">
+        <h3>Cube Summation - Coding challenge by HackerRank</h3>
+        <p class="lead">Official instructions can be found <a href="https://www.hackerrank.com/challenges/cube-summation" target="_blank">here</a></p>
+        <p><a class="btn btn-lg btn-success" href="#" role="button">Start Challenge</a></p>
+      </div>
+      <footer class="footer">
+        <p>&copy; 2016 Manuel F. Stroh S.</p>
+      </footer>
 
-    </div><!-- /.container-fluid -->
-</nav>
+    </div>
 
-<div class="container" id="main">
+<div class="main-console" id="main">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             @yield('content')
         </div>
     </div>
-
 </div>
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-<script src="http://cdnjs.cloudflare.com/ajax/libs/vue/1.0.14/vue.min.js"></script>
-<script src="js/app.js"></script>
 </body>
 </html>
